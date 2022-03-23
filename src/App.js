6f2bce006c3f4160428efe8305c60b4278adb416
Aldahir Mayorga Garcia <samg.25sep@gmail.com>
2022-03-23 13:36:45 -0600

    Clase 2: introduccion a props, componentes y conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '../src/components/Button'
+
+function Header() {
+  return (
+
+
+    <header className="header">
+      <span classname="logo"> Logo</span>
+      <h3>Este es el header</h3>
+      <div className="containerLinks">
+        <a href="# "> <span> Home</span></a>
+        <a href="# "> <span> Acerca de </span></a>
+        <a href="# "> <span> Contacto</span></a>
+      </div>
+    </header>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>Hecho con con 3 en Kodemia por {props.name}</footer>
+  );
+
+}
+
+function Layout(props) {
+  return (
+    <div className="layout">
+      <Header />
+        {props.children}
+      <Footer />
+    </div>
+
+  );
+}
+
 
 function App() {
+  const name = 'Aldahir Mayorga'
+  const age = '27'
+  const ageCopy = `Tengo ${age} años`
   return (
     <div className="App">
-      <header className="App-header">
+      <Layout >
+      <main className="App-header">
+        <h2>Hol soy {name}</h2>
+        <p>Me gustan los gatos y tengo {age} años</p>
+        <p>{ageCopy}</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Button color= "green"  >Texto Boton</Button>
+      <Button color= "blue"  >Texto </Button>
+      <Button color="yellow"  >Texto Hi</Button>
+      </main>
+      </Layout>
+      
+     
     </div>
   );
 }
